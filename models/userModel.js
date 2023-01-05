a603c4e110f7b978b448e9895b5431e4f79e3af3
Rezaul <mdrezaulkarrim@gmail.com>
2023-01-05 11:28:36 +0600

    restricted route implemented

--- models/userModel.js
+++ models/userModel.js
@@ -1,43 +1,6 @@
 const validator = require('validator');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-const AppError = require('../utils/appError');
-
-// const userSchema = new mongoose.Schema({
-//   name: {
-//     type: String,
-//     required: [true, 'Please tell us your name'],
-//     trim: true,
-//   },
-//   email: {
-//     type: String,
-//     lowerCase: true,
-//     unique: true,
-//     validate: [validator.isEmail, 'Please provide a valid email'],
-//     required: [true, 'Please provide your email'],
-//   },
-//   photo: String,
-//   password: {
-//     type: String,
-//     minLength: 8,
-//     required: [true, 'Please provide  a password'],
-//     select: false,
-//   },
-//   passwordConfirm: {
-//     type: String,
-//     required: [true, 'Please confirm your password'],
-//     // Validators triggers on Create and Save method
-//     validate: {
-//       validator: function (el) {
-//         return el === this.password;
-//       },
-//       message: 'Passwords are not the same',
-//     },
-//   },
-//   passwordChangedAt: Date,
-// });
-
-// const User = mongoose.model('User', userSchema);
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -52,6 +15,11 @@ const userSchema = new mongoose.Schema({
     validate: [validator.isEmail, 'Please provide a valid email'],
   },
   photo: String,
+  role: {
+    type: String,
+    enum: ['user', 'guide', 'lead-guide', 'admin'],
+    default: 'user',
+  },
   password: {
     type: String,
     required: [true, 'Please provide a valid password'],
@@ -65,7 +33,7 @@ const userSchema = new mongoose.Schema({
       validator: function (el) {
         return el === this.password;
       },
-      message: 'Password are not the same    ',
+      message: 'Password are not the same',
     },
   },
   passwordChangedAt: Date,
